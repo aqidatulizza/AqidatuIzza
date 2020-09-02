1f63b201d7ff631afd4ae2b441e7f435bed04590
aqidatulizza <69777827+aqidatulizza@users.noreply.github.com>
2020-09-02 22:36:07 +0700

    backend

---
+++ about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Web Resep Makanan </title>
+	<link rel="stylesheet" type="text/css" href="tampilan.css">
+</head>
+<body bgcolor="black">
+
+	<div class="header">
+		<h1><center> DapurQoe </center></h1>
+	</div>
+	<div class="menu">
+		<ul id="navigasi">
+		<li><a href="home.php"><b>Home</a></li>
+		<li><a href="#"><b>Category</a>
+		<ul>
+		<li><a href="kategoriayam.php"><b>Ayam</a></li>
+		<li><a href="kategoricamilan.php"><b>Camilan</a></li>
+		</ul>
+		<li><a href="#"><b>About</a></li>
+		<li><a href="#"><b>Account</a>
+		<ul>
+		<li><a href="index.php"><b>Log Out</a></li>
+		</ul>
+		</li>
+		</ul>
+	</div>
+	<div class="kotak1">
+		<h2><b> Tentang Kami </b></h2>
+		<div class="kotak2">
+			DapurQue adalah sebuah website yang berisi kumpulan resep masakan manis maupun asin. website ini menyediakan bantuan bagi Anda yang membutuhkan bimbingan memasak yang mudah dan simple.
+		</div>
